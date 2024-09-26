3ef82543125557888f6c60411afdbfa58340e5bd
DESKTOP-P7IVN6B\krallen <adanx05@gmail.com>
2024-09-26 08:47:28 +0200

    fix blackList

--- src/main/java/com/debuggeando_ideas/best_travel/infraestructure/helper/BlackListHelper.java
+++ src/main/java/com/debuggeando_ideas/best_travel/infraestructure/helper/BlackListHelper.java
@@ -1,7 +1,14 @@
 package com.debuggeando_ideas.best_travel.infraestructure.helper;
 
 import com.debuggeando_ideas.best_travel.util.exceptions.ForbiddenCustomerException;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
+@Component
+@Slf4j
+@AllArgsConstructor
 public class BlackListHelper {
 
     public void isInBlackListCustomer(String customerId){
